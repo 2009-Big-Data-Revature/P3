9438b6ffda0adc668dd4bdabd05bd05aa062cd72
Miguel Pena <miguel674@revature.net>
2024-08-22 08:12:32 -0700

    restructuring

---
+++ Year_2000/datadumper.py
@@ -0,0 +1,110 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import zipfile
+from tqdm import tqdm
+import threading
+
+# Function to download a zip file
+def download_zip(link, location):
+    zip_file_response = requests.get(link)
+    status_code = zip_file_response.status_code
+
+    print(f"Status Code: {status_code}")
+
+    if status_code == 200:
+        # Extract the filename from the URL
+        filename = os.path.basename(link)
+        # Construct the full path to save the file
+        file_path = os.path.join(location, filename)
+        
+        # Save the file content
+        with open(file_path, 'wb') as f:
+            f.write(zip_file_response.content)
+            print(f"File downloaded and saved as {file_path}")
+    else:
+        print(f"Failed to download the file. Status code: {status_code}")
+
+# Function to start downloading files in threads
+def download_files_in_threads(url_list, location):
+    threads = []
+    for url in url_list:
+        thread = threading.Thread(target=download_zip, args=(url, location))
+        thread.start()
+        threads.append(thread)
+    
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
+# Function to extract a zip file
+def extract_zip(zip_file, extract_location):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extract_location)
+        print(f"Extracted {zip_file} into {extract_location}")
+
+# Function to start extracting files in threads
+def extract_files_in_threads(zip_files, extract_location):
+    threads = []
+    for zip_file in zip_files:
+        thread = threading.Thread(target=extract_zip, args=(zip_file, extract_location))
+        thread.start()
+        threads.append(thread)
+    
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
+# Function to dump data from a base URL
+def dump_data(base_url, location):
+    r = requests.get(base_url)
+    status_code = r.status_code
+    print(f"Status Code: {status_code}")
+
+    if status_code == 200:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        zip_urls = []
+        for anchor in soup.find_all('a'):
+            href = anchor.get('href')
+            if href and href.endswith('.zip'):
+                zip_file_name = href
+                print(f"Found file: {zip_file_name}")
+                zip_urls.append(base_url + zip_file_name)
+
+        # Download all zip files concurrently
+        download_files_in_threads(zip_urls, location)
+    elif status_code == 404:
+        print(f"Error 404 Not Found For {base_url}")
+
+if __name__ == "__main__":
+    year = '2000'
+    states = ["Alabama", "Alaska", "Arizona", "Arkansas", \
+              "California", "Colorado", "Connecticut", \
+              "Delaware", "District_of_Columbia", "Florida", \
+              "Georgia", "Hawaii", "Idaho", "Illinois", \
+              "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", \
+              "Maine", "Maryland", "Massachusetts", "Michigan", \
+              "Minnesota", "Mississippi", "Missouri", "Montana", \
+              "Nebraska", "Nevada", "New_Hampshire", "New_Jersey", \
+              "New_Mexico", "New_York", "North_Carolina", \
+              "North_Dakota", "Ohio", "Oklahoma", "Oregon", \
+              "Pennsylvania", "Puerto_Rico", "Rhode_Island", \
+              "South_Carolina", "South_Dakota", "Tennessee", \
+              "Texas", "Utah", "Vermont", "Virginia", "Washington", \
+              "West_Virginia", "Wisconsin", "Wyoming"]
+
+    # Will dump all the data first
+    for state in tqdm(states):
+        state = state.replace(' ', '_')
+        os.makedirs(f"dump/{state}", exist_ok=True)
+        os.makedirs(f"dump/extracted_data/{state}", exist_ok=True)
+        match year:
+            case "2010":
+                dump_data('https://www2.census.gov/census_' + year + '/redistricting_file--pl_94-171/' + state +'/', f'dump/{state}')
+            case "2000":
+                dump_data('https://www2.census.gov/census_' + year + '/datasets/redistricting_file--pl_94-171/' + state +'/', f'dump/{state}')
+
+    # Then it will extract all the folders
+    for state in tqdm(states):
+        zip_files = [os.path.join(f'dump/{state}', f) for f in os.listdir(f'dump/{state}') if f.endswith('.zip')]
+        extract_files_in_threads(zip_files, f'dump/extracted_data/{state}')
---
+++ Year_2020/scraper.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class HeaderScraper:
+    def __init__(self):
+        self.tables = {}
+        pass
+
+    def scrape(self):
+        URL = r"https://www2.census.gov/census_2000/datasets/redistricting_file--pl_94-171/0File_Structure/File_Structure_README.htm"
+        r = requests.get(URL)
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        tables = soup.find_all('table')
+
+        #Scrapes table PL_GEOHD (Geo Headers)
+        geo_list = []
+        for i, row in enumerate(tables[0].find_all('tr')):
+            geo_list.append([el.text.strip() for el in row.find_all('td')])
+        self.tables['geo'] = pd.DataFrame(geo_list[1:], columns = geo_list[0])
+
+        #Scrapes table PL_PART1
+        p1_list = []
+        for i, row in enumerate(tables[1].find_all('tr')):
+            p1_list.append([el.text.strip() for el in row.find_all('td')])
+        self.tables['p1'] = pd.DataFrame(p1_list[1:], columns = p1_list[0])
+
+        #Scrapes table PL_PART2
+        p2_list = []
+        for i, row in enumerate(tables[2].find_all('tr')):
+            p2_list.append([el.text.strip().replace('\r', '').replace('\n', '') for el in row.find_all('td')])
+        self.tables['p2'] = pd.DataFrame(p2_list[1:], columns = p2_list[0])
+
+        #Scrapes table HEADERS
+        headers_list = []
+        for i, row in enumerate(tables[3].find_all('tr')):
+            headers_list.append([el.text.strip().replace('\r', '').replace('\n', '') for el in row.find_all('td')])
+        self.headers_df = pd.DataFrame(headers_list[1:], columns = headers_list[0])
+
+        #Scrapes TABLES list and splits it into race, hisp/lat, race > 18, and hisp/lat > 18 tables
+        tables_list = []
+        for i, row in enumerate(tables[4].find_all('tr')):
+            tables_list.append([el.text.strip().replace('\r', '').replace('\n', '') for el in row.find_all('td')])
+        tables_df = pd.DataFrame(tables_list[1:], columns = tables_list[0])
+        race_index = tables_df.index[tables_df.STUB.str.match('^PL1')].tolist()[0]
+        hisp_index = tables_df.index[tables_df.STUB.str.match('^PL2')].tolist()[0]
+        race18_index = tables_df.index[tables_df.STUB.str.match('^PL3')].tolist()[0]
+        hisp18_index = tables_df.index[tables_df.STUB.str.match('^PL4')].tolist()[0]
+
+
+        self.tables['race'] = tables_df[race_index+2:hisp_index]
+        self.tables['hisp'] = tables_df[hisp_index+2:race18_index]
+        self.tables['race18'] = tables_df[race18_index+2:hisp18_index]
+        self.tables['hisp18'] = tables_df[hisp18_index+2:]
+    
+    def __getitem__(self, category: str):
+        #Allows object of class to be indexed by name of desired table (ex: HeaderScraper['geo'] returns the dataframe for the geo headers)
+        return self.tables[category]
