6cecf0d218a7d839444221b2514f792a55ffe352
miguelpena-bigdata-dev <miguel674@revature.net>
2024-08-22 13:57:19 -0700

    tony fix for utf-8 error

--- Year_2010/oluwatobiCleaner.py
+++ Year_2010/oluwatobiCleaner.py
@@ -58,7 +58,7 @@ def getRows(lines, rows):
 
 #Open folder
 def open_Folder(file_path):
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8", errors="replace") as file:
         lis = file.read()
     return lis
 
