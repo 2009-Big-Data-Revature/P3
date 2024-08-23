9551cf4924f48f782af126074b9ab08fb1c0e33e
manders-12 <michaela121999@gmail.com>
2024-08-23 09:35:58 -0500

    Full combiner script added

--- Year_2000/fast_combine.py
+++ Year_2000/fast_combine.py
@@ -4,23 +4,20 @@ import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
-DUMP_PATH = 'C:/Users/Hunter/Desktop/Revature/P3/dump/extracted_data'
-OUT_PATH = 'C:/Users/Hunter/Desktop/Revature/P3/out.csv'
+#INSERT PATHS
+#DUMP_PATH = '.../extracted_data'
+#OUT_PATH = '.../out.csv'
 
+#Joins geo and other table
 def tableConcat(headers: dict, geo: pd.DataFrame, table: pd.DataFrame) -> pd.DataFrame:
-    geo_headers = headers['geo']['Name'].to_list()
+    geo_headers = headers['geo']['Name'][6:8].to_list()
     geo.columns = geo_headers
-
-    #merged = tables[0].copy()
-    #tables[0].columns = headers['p1']['Name'].to_list()
-    #tables[0] = tables[0].loc[:,['STUSAB', 'LOGRECNO', 'P0010001', 'P0010003', 'P0010004', 'P0010005', 'P0010006', 'P0010007', 'P0010008', 'P0010009', 'P0020002', 'P0020003']]
     trunc_geo = geo.loc[:,['LOGRECNO', 'REGION']]
 
     return pd.merge(table, trunc_geo, how = 'inner', on = ['LOGRECNO'])
 
 
 def process_directory(dir, headers, colspecs):
-    start = time.time()
     base = os.path.join(DUMP_PATH, dir)
     files = [os.fsdecode(file) for file in os.listdir(base)]
 
@@ -30,7 +27,7 @@ def process_directory(dir, headers, colspecs):
                                           'P0010003', 'P0010004', 'P0010005', 
                                           'P0010006', 'P0010007', 'P0010008', 
                                           'P0010009', 'P0020002', 'P0020003'], 
-                          usecols = [1, 4, 6, 7, 8, 9, 10, 11, 12, 13, 77, 78])
+                          usecols = [1, 4, 6, 7, 8, 9, 10, 11, 12, 13, 77, 78]) #Double check these column numbers
     
     #Creating geo dataframe - this one is more complicated because it's a fwf instead of a csv
     path = os.path.join(base, files[2])
@@ -39,9 +36,9 @@ def process_directory(dir, headers, colspecs):
     if(files[0] == 'pr00001.upl'):
         geo = pd.read_fwf(path, colspecs=colspecs, header = None, encoding = 'latin1', memory_map = True, usecols = [6, 7])
     else:
-        geo = pd.read_fwf(path, colspecs=colspecs, header = None, memory_map = True)
+        geo = pd.read_fwf(path, colspecs=colspecs, header = None, memory_map = True, usecols = [6, 7])
 
-    return start, tableConcat(headers, geo, table_1)
+    return tableConcat(headers, geo, table_1)
 
 #Scraping headers
 scraper = HeaderScraper()
@@ -65,11 +62,8 @@ with ThreadPoolExecutor() as executor:
     for future in as_completed(futures):
         dir = futures[future]
         try:
-            start, combined = future.result()
-            combined.drop(columns = ['LOGRECNO']).to_csv(OUT_PATH, mode = 'a', index = False, header = False)
-            end = time.time()
-            #full = pd.concat([full, combined.drop(columns = ['LOGRECNO'])])
-            print(f"{dir} finished: {combined.shape[0]} rows appended. Time: {end - start}")
+            combined = future.result()
+            combined.drop(columns = ['LOGRECNO']).to_csv(OUT_PATH, mode = 'a', index = False, header = False) #Appends to out.csv
+            print(f"{dir} finished: {combined.shape[0]} rows appended.")
         except Exception as e:
             print(f"Error processing directory {dir}: {e}")
-#full.to_csv(OUT_PATH)
